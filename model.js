b7871175cff81d113c381c5962efc897c0f26cdb
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-10-26 21:49:15 +0100

    Model setup

---
+++ model.js
@@ -0,0 +1,29 @@
+const model = {
+    userid: "",
+    username: "",
+    password: "",
+    firstlogin: "",
+    date: "",
+
+    
+    animalid: "", 
+    animalname: "",
+    birthdate: "", 
+    feeding: "", 
+    bathing: "", 
+    growstage: [], //egg,child,grown,special
+
+    animals: [],
+
+    lastlogin: "", 
+    nextloginlimit: "", 
+    lastfeeding: "",
+    nextfeedinglimit: "",
+    lastplay: "",
+    nextplay: "", 
+    lastbath: "", 
+    nextbathlimit: "",
+
+    userlist: [],
+    tamalist: [],
+}
