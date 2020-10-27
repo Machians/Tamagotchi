efd83f70be686a2671c72edd280387d1afe25c6e
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-10-27 10:53:29 +0100

    hmm form onsubmit?

--- controller.js
+++ controller.js
@@ -1,12 +1,10 @@
-let username = document.getElementById('username');
-let password = document.getElementById('password');
+function login() {
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
 
-if (username.valid && password.valid == true) {
-    function login() {
-        let login = document.getElementById('login');
-        login.style.display = "none";
-        model.username = username;
-        model.password = password;
-        console.log(model.username);
-    } 
-    }else login.style.display ="flex";
+  if (username.required && password.required == true) {
+    let login = document.getElementById("login");
+    login.style.display = "none";
+  }
+  updateView();
+}
--- view.js
+++ view.js
@@ -2,10 +2,10 @@ function updateView() {
     document.getElementById('container').innerHTML = `
     <div id="header">Tamagotchi</div>
 
-    <form id="login">
-        <input id="username" class="submit" type="text" minlength="1" required/>
-        <input id="password" class="submit" type="password" minlength="1" required/>
-        <button id="loginbutton" class="submit" onclick="login()" type="button">Log in</button>
+    <form onsubmit="login():return false" id="login">
+        <input id="username" class="submit" type="text" placeholder="Brukernavn" required/>
+        <input id="password" class="submit" type="password" placeholder="Passord" required/>
+        <button id="loginbutton" class="submit">Log in</button>
     </form>
 
     <div id="window">
