01a395fe60b76ba9f266590d1d5b36df1acdd2b4
Poablin <65161335+Poablin@users.noreply.github.com>
2020-10-29 14:46:37 +0100

    Create model.js

---
+++ model.js
@@ -0,0 +1,34 @@
+const model = {
+  loggedin: "", //"yes"/"no"
+  userid: "",
+  username: "",
+  password: "",
+  firstlogin: "",
+  date: "",
+
+  animalid: "",
+  animalname: "",
+  birthdate: "",
+  feeding: "",
+  needsbathing: "", //yes/no
+  growstage: "", //egg,child,grown,special
+  dead: "", //yes/no
+  album: ["", "", "", ""], //should contain growth history in images.
+
+  animals: [],
+
+  lastlogin: "",
+  nextloginlimit: "",
+  lastfeeding: "",
+  nextfeedinglimit: "",
+  lastplay: "",
+  nextplay: "",
+  lastbath: "",
+  nextbathlimit: "",
+
+  menuopen: "no", //"yes"/"no"
+  foodmenuopen: "no", //"yes"/"no"
+
+  userlist: [],
+  tamalist: [],
+};
